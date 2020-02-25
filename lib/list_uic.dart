b85b5ffd0c82a22e0f5226b6e064f81f3cebed31
Evgeny Cherkasov <ech89899@gmail.com>
2020-02-25 17:36:26 +0300

    'ListUic' basic states

--- lib/list_uic.dart
+++ lib/list_uic.dart
@@ -3,79 +3,103 @@ library list_uic;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'progress_uic.dart';
+
 /// Default empty view shows icon and text in the center of screen. You can
-/// provide your own icon and/or text in [emptyIcon] and [emptyText] parameters.
-/// To use your custom view for empty state set [emptyView] parameter.
+/// provide your own icon and/or text in [emptyDataIcon] and [emptyDataText] parameters.
+/// To use your custom view for empty state set [emptyDataView] parameter.
 ///
 /// See also:
 ///
 /// * [ListUicEmptyDataView]
+/// * [ListUicEmptyProgressView]
+///
 class ListUic<T> extends StatelessWidget {
   ListUic({
     Key key,
     @required this.controller,
     @required this.itemBuilder,
-    this.emptyIcon = const Icon(Icons.sentiment_dissatisfied, size: 96.0,),
-    this.emptyText = 'No results',
-    Widget emptyView,
-  }) : assert(emptyView != null || emptyText != null),
-        emptyView = emptyView ?? ListUicEmptyDataView(icon: emptyIcon, text: emptyText),
+    this.emptyDataIcon = const Icon(Icons.sentiment_dissatisfied,
+      size: 96.0, color: Colors.black26,),
+    this.emptyDataText = 'No results',
+    Widget emptyDataView,
+    this.emptyErrorIcon = const Icon(Icons.error_outline,
+      size: 96.0, color: Colors.black26,),
+    this.emptyErrorText = 'Error loading data',
+    Widget emptyErrorView,
+    this.emptyProgressText = 'Loading...',
+    Widget emptyProgressView,
+  }) : assert(emptyDataView != null || emptyDataText != null),
+        emptyDataView = emptyDataView ?? ListUicEmptyDataView(
+            controller: controller,
+            icon: emptyDataIcon,
+            text: emptyDataText),
+        emptyErrorView = emptyDataView ?? ListUicEmptyDataView(
+            controller: controller,
+            icon: emptyErrorIcon,
+            text: emptyErrorText),
+        emptyProgressView = emptyProgressView ?? ListUicEmptyProgressView(text: emptyProgressText),
         super(key: key);
 
-  final ListUicController controller;
+  ///
+  final ListUicController<T> controller;
 
   ///
-  final IndexedWidgetBuilder itemBuilder;
+  final Widget Function(T item) itemBuilder;
 
-  /// Icon to display in default empty view
-  ///
-  /// See also:
-  ///
-  /// * [ListUicEmptyDataView]
-  final Icon emptyIcon;
+  /// Icon to display in default empty data view
+  final Icon emptyDataIcon;
 
-  /// Text to display in default empty view
-  final String emptyText;
+  /// Text to display in default empty data view
+  final String emptyDataText;
 
   /// View to display when the list is empty.
-  final Widget emptyView;
+  final Widget emptyDataView;
+
+  /// Icon to display in default empty error view
+  final Icon emptyErrorIcon;
+
+  /// Text to display in default empty error view
+  final String emptyErrorText;
+
+  /// View to display when the initial data loading is failed.
+  final Widget emptyErrorView;
+
+  /// Text to display in default empty progress view
+  final String emptyProgressText;
+
+  /// View to display when the initial data loading is in progress.
+  final Widget emptyProgressView;
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider.value(
-      value: controller.items,
-      child: Consumer<ValueNotifier<StatefulData<List<T>>>>(
-        builder: (context, data, child) {
-          return Stack(
-            children: <Widget>[
-              if (data.value.isEmpty && !data.value.isLoading)
-                emptyView,
-              if (!data.value.isEmpty)
-                ListUicDataView(
-                  itemBuilder: itemBuilder,),
-
-            ],
-          );
+      value: controller.state,
+      child: Consumer<ValueNotifier<ListUicState>>(
+        builder: (context, state, child) {
+          switch (state.value) {
+            case ListUicState.emptyData:
+              return emptyDataView;
+            case ListUicState.emptyProgress:
+              return emptyProgressView;
+            case ListUicState.emptyError:
+              return emptyErrorView;
+            case ListUicState.data:
+              return _dataView();
+            default:
+              return Container();
+          }
         },
       ),
     );
   }
 
-}
-
-class ListUicDataView extends StatelessWidget {
-  const ListUicDataView({
-    Key key,
-    this.itemBuilder,
-  }) : super(key: key);
-
-  ///
-  final IndexedWidgetBuilder itemBuilder;
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _dataView() {
     return ListView.builder(
-        itemBuilder: itemBuilder,
+      itemCount: controller.items.value.length,
+      itemBuilder: (context, index) {
+        return itemBuilder(controller.items.value[index]);
+      },
     );
   }
 }
@@ -83,9 +107,12 @@ class ListUicDataView extends StatelessWidget {
 class ListUicEmptyDataView extends StatelessWidget {
   const ListUicEmptyDataView({
     Key key,
+    @required this.controller,
     this.icon,
     this.text,
   }) : super(key: key);
+
+  final ListUicController controller;
 
   final Icon icon;
 
@@ -95,10 +122,21 @@ class ListUicEmptyDataView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           icon,
-          Text(text,
-            style: Theme.of(context).textTheme.headline,
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Text(text,
+              style: Theme.of(context).textTheme.headline,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: RaisedButton(
+              child: Text('Refresh'),
+              onPressed: () => controller.refresh(),
+            ),
           ),
         ],
       ),
@@ -106,60 +144,93 @@ class ListUicEmptyDataView extends StatelessWidget {
   }
 }
 
+class ListUicEmptyProgressView extends StatelessWidget {
+  const ListUicEmptyProgressView({
+    Key key,
+    this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ProgressUic(
+      text: text,
+    );
+  }
+}
+
 class ListUicController<T> {
   ListUicController({
-    List<T> items
-  });
+    List<T> items,
+    @required this.onGetItems,
+    this.initialLoading = true,
+  }) {
+    _items = ValueNotifier(items ?? List());
+    _page = 1;
+    if (_items.value.isEmpty) {
+      if (initialLoading) {
+        _state = ValueNotifier(ListUicState.emptyProgress);
+        refresh();
+      }
+      else {
+        _state = ValueNotifier(ListUicState.emptyData);
+      }
+    }
+    else {
+      _state = ValueNotifier(ListUicState.data);
+    }
+  }
 
-  ValueNotifier<StatefulData<List<T>>> _items;
+  ValueNotifier<ListUicState> _state;
+  ValueNotifier<ListUicState> get state => _state;
 
-  ValueNotifier<StatefulData<List<T>>> get items => _items;
+  ValueNotifier<List<T>> _items;
+  ValueNotifier<List<T>> get items => _items;
 
-  int _page = 1;
+  bool initialLoading;
 
-  Function getItems;
+  int _page;
+
+  Future<List<T>> Function(int) onGetItems;
 
   Future<void> refresh() async {
+    // Show progress view
+    if (_state.value == ListUicState.emptyData
+          || _state.value == ListUicState.emptyError) {
+      _state.value = ListUicState.emptyProgress;
+    }
+    // Load first page of the data
     _page = 1;
-    List<T> result = await getItems(_page);
-//    items = result;
+    _loadItems()
+        // Show data
+        .then((result) {
+          _items.value = result;
+          if (_items.value.isEmpty) {
+            _state.value = ListUicState.emptyData;
+          }
+          else {
+            _state.value = ListUicState.data;
+          }
+        })
+        // Show error
+        .catchError((error) {
+          _state.value = ListUicState.emptyError;
+        });
   }
 
   Future<void> nextPage() async {
     _page++;
-    List<T> result = await getItems(_page);
-//    items.addAll(result);
+    List<T> result = await _loadItems();
+    result.addAll(await onGetItems(_page));
+    items.value = result;
+  }
+
+  Future<List<T>> _loadItems() async {
+    return await onGetItems(_page) ?? List();
   }
 }
 
-class StatefulData<T> {
-  StatefulData({
-    this.data,
-    this.isEmpty,
-    this.isLoading,
-    this.isError,
-    this.error,
-  });
-
-  T data;
-  bool isEmpty;
-  bool isLoading;
-  bool isError;
-  String error;
-
-  StatefulData<T> copyWith({
-    T data,
-    bool isEmpty,
-    bool isLoading,
-    bool isError,
-    String error,
-  }) {
-    return StatefulData(
-      data: data ?? this.data,
-      isEmpty: isEmpty ?? this.isEmpty,
-      isLoading: isLoading ?? this.isLoading,
-      isError: isError ?? this.isError,
-      error: error ?? this.error,
-    );
-  }
-}
+enum ListUicState { init,
+                      emptyData, emptyProgress, emptyError,
+                      data, progress, error }
