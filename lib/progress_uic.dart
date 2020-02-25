e94cdc702134805ed4798305ecda31776d9155c4
Evgeny Cherkasov <ech89899@gmail.com>
2020-02-25 17:36:58 +0300

    'ProgressUic' implementation

---
+++ lib/progress_uic.dart
@@ -0,0 +1,32 @@
+library list_uic;
+
+import 'package:flutter/material.dart';
+
+class ProgressUic extends StatelessWidget {
+  ProgressUic({
+    Key key,
+    this.text = '',
+  }) :
+    super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircularProgressIndicator(),
+          if (text.isNotEmpty)
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: Text(text,
+                style: Theme.of(context).textTheme.headline,
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
