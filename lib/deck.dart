dca23a2643feafdaba645bf72997792a7be2e52e
Evgeny Cherkasov <ech89899@gmail.com>
2020-10-28 19:40:55 +0300

    Refactor to update widget size when items are expanded/collapsed

--- lib/deck.dart
+++ lib/deck.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-class Deck extends StatelessWidget {
+class Deck extends StatefulWidget {
   Deck({
     Key key,
     this.collapsedSize = 48.0,
@@ -19,44 +19,59 @@ class Deck extends StatelessWidget {
   final double mainAxisSize;
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: We should honor 'Deck' orientation to apply 'mainAxisSize' either on height or width
-    double eventualHeight = mainAxisSize ?? MediaQuery.of(context).size.height / 2;
-    double eventualExpandedSize = expandedSize ?? eventualHeight / items.length;
-    DeckData data = DeckData(
-      height: eventualHeight,
-      animationValues: items.map((item) => 0.0).toList(),
-      expandedStates: Map.fromIterable(items, key: (item) => item, value: (item) => false),
-      items: items,
-    );
-    return Container(
-      height: eventualHeight,
-//      child: SingleChildScrollView(
-        child: Container(
-          height: items.length * eventualExpandedSize,
-          child: Flow(
-            delegate: DeckFlowDelegate(
-              collapsedSize: collapsedSize,
-              expandedSize: eventualExpandedSize,
-              data: data,
-            ),
-            children: [...items.map((item) => _DeckItem(
-              child: item.child,
-              childExpanded: item.childExpanded,
-              index: items.indexOf(item),
-              animationListener: (double value) {
-                data.setAnimationValue(items.indexOf(item), value);
-              },
-              expandedListener: (bool value) {
-                data.setExpanded(item, value);
-              },
-            ))],
-          ),
-        ),
-//      ),
+  _DeckState createState() => _DeckState();
+}
+
+class _DeckState extends State<Deck> with TickerProviderStateMixin {
+
+  DeckData _data;
+
+  double _eventualHeight;
+
+  @override
+  void initState() {
+    super.initState();
+    _data = DeckData(
+      items: widget.items,
+      vsync: this,
     );
   }
 
+  @override
+  Widget build(BuildContext context) {
+    // TODO: We should honor 'Deck' orientation to apply 'mainAxisSize' either on height or width
+    _eventualHeight = widget.mainAxisSize ?? MediaQuery.of(context).size.height / 2;
+    double eventualExpandedSize = widget.expandedSize ?? _eventualHeight / widget.items.length;
+    return Container(
+        constraints: BoxConstraints(
+          minHeight: widget.collapsedSize * widget.items.length,
+          maxHeight: _eventualHeight,
+        ),
+//      child: SingleChildScrollView(
+//        child: Container(
+//          height: items.length * eventualExpandedSize,
+          child: Flow(
+            delegate: DeckFlowDelegate(
+              collapsedSize: widget.collapsedSize,
+              expandedSize: eventualExpandedSize,
+              data: _data,
+            ),
+            children: [...widget.items.map((item) =>
+              GestureDetector(
+                child: _data.expandedStates[item] ? item.childExpanded ?? item.child : item.child,
+                onTap: () {
+                  setState(() {
+                    _data.setExpanded(item, !_data.expandedStates[item]);
+                    _data.expandedStates[item] ?
+                      _data.animations[item].forward() : _data.animations[item].reverse();
+                  });
+                }),
+              )],
+          ),
+//        ),
+//      ),
+    );
+  }
 }
 
 class DeckFlowDelegate extends FlowDelegate {
@@ -64,19 +79,34 @@ class DeckFlowDelegate extends FlowDelegate {
     @required this.data,
     this.collapsedSize,
     this.expandedSize,
-  }) : super(repaint: data);
-
-  final DeckData data;
+  }) : _expandedStates = Map.from(data.expandedStates),
+        super(repaint: data);
 
   final double collapsedSize;
 
+  final DeckData data;
+
   final double expandedSize;
+
+  final Map<DeckItem, bool> _expandedStates;
+
+  @override
+  Size getSize(BoxConstraints constraints) {
+    print('getSize');
+    double childrenHeight = data.items
+        .map((item) => (data.expandedStates[item]) ?
+          max(expandedSize, collapsedSize) :
+          collapsedSize)
+        .fold(0.0, (previousValue, element) => previousValue + element);
+    return Size(constraints.maxWidth, min(constraints.maxHeight, childrenHeight));
+  }
 
   @override
   void paintChildren(FlowPaintingContext context) {
+    print('paintChildren');
     List<double> childrenHeight = data.items
         .map((item) => (data.expandedStates[item]) ?
-          max(expandedSize * data.animationValues[data.items.indexOf(item)], collapsedSize) :
+          max(expandedSize * data.animations[item].value, collapsedSize) :
           collapsedSize)
         .toList();
     for (int i = 0; i < context.childCount; ++i) {
@@ -92,7 +122,18 @@ class DeckFlowDelegate extends FlowDelegate {
   }
 
   @override
+  bool shouldRelayout(DeckFlowDelegate oldDelegate) {
+    for (DeckItem item in data.expandedStates.keys) {
+      if (data.expandedStates[item] != oldDelegate._expandedStates[item])
+        return true;
+    }
+    print('shouldRelayout(): false');
+    return false;
+  }
+
+  @override
   bool shouldRepaint(DeckFlowDelegate oldDelegate) {
+    print('shouldRepaint');
     return false;
   }
 }
@@ -109,81 +150,44 @@ class DeckItem {
   final Widget childExpanded;
 }
 
-class _DeckItem extends StatefulWidget {
-  _DeckItem({
-    Key key,
-    this.animationListener,
-    this.child,
-    this.childExpanded,
-    this.expandedListener,
-    this.index,
-  });
-
-  final void Function(double value) animationListener;
-
-  final Widget child;
-
-  final Widget childExpanded;
-
-  final void Function(bool value) expandedListener;
-
-  final int index;
-
-  @override
-  State<_DeckItem> createState() => _DeckItemState();
-}
-
-class _DeckItemState extends State<_DeckItem> with SingleTickerProviderStateMixin {
-
-  AnimationController _animation;
-
-  bool _isExpanded = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _animation = AnimationController(
-      duration: const Duration(milliseconds: 300),
-      vsync: this,
-    );
-    _animation.addListener(() => widget.animationListener(_animation.value));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      child: _isExpanded ? widget.childExpanded : widget.child,
-      onTap: () {
-        setState(() {
-          _isExpanded = !_isExpanded;
-          _isExpanded ? _animation.forward() : _animation.reverse();
-          widget.expandedListener(_isExpanded);
-        });
-      },
-    );
-  }
-}
-
 class DeckData extends ChangeNotifier {
   DeckData({
-    this.animationValues,
-    this.expandedStates,
-    this.height,
+//    this.animationValues,
+//    this.animations,
+//    this.expandedStates,
+//    this.height,
     this.items,
-  });
+    this.vsync,
+  }) {
+    animations = Map.fromIterable(items, key: (item) => item,
+      value: (item) {
+        AnimationController animation = AnimationController(
+          duration: const Duration(milliseconds: 300),
+          vsync: vsync,
+        );
+        animation.addListener(() => notifyListeners());
+        return animation;
+      }
+    );
+    expandedStates = Map.fromIterable(items, key: (item) => item, value: (item) => false);
+  }
 
-  final List<double> animationValues;
+//  final List<double> animationValues;
 
-  final Map<DeckItem, bool> expandedStates;
+  Map<DeckItem, AnimationController> animations;
 
-  final double height;
+  Map<DeckItem, bool> expandedStates;
+
+//  final double height;
 
   final List<DeckItem> items;
 
-  void setAnimationValue(int i, double value) {
-    animationValues[i] = value;
-    notifyListeners();
-  }
+  final TickerProvider vsync;
+
+//  void setAnimationValue(int i, double value) {
+//    animationValues[i] = value;
+//    notifyListeners();
+//  }
 
   void setExpanded(DeckItem item, bool value) {
     expandedStates[item] = value;
